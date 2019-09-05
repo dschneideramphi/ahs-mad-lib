d84908d817905114b56ba5d76075eb96a42d7fad
dschneider <dschneider@amphi.com>
2019-09-05 16:56:50 -0700

    created assignment

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+  Code you will need:
+  let ___ = document.querySelector()
+  ___.addEventListener("click", (e) => {
+
+  });
+  let ___ = prompt("");
+  ___.innerHTML = "";
+  `${___}`
+
+*/
+
+//Step 1: Use prompt to ask the user for 5 things
+
+//Step 2: Get each div element using querySelector
+
+//Step 3: Use .innerHTML and ${__} to fill each div element with the rule string
+
+//Step 4: Celebrate
+
+//Step 5: Add _style_!
